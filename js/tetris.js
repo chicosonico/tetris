28dc01f0d306e9ab138e3f602adeb49e2beaf7c6
Pablo Schvarzman <pablonet@Pablos-MacBook-Pro.local>
2020-03-22 17:38:32 +0100

    canvas reasy

--- js/tetris.js
+++ js/tetris.js
@@ -2,10 +2,14 @@ var canvas;
 var ctx;
 var FPS = 50;
 
-var weightGameTable = 10;
+var widthCanvas = 400;
+var heightCanvas = 640;
+
+
+var widthGameTable = 10;
 var heightGameTable = 10;
 
-var weightPiece = 40;
+var widthPiece = 40;
 var heightPiece = 40;
 
 
@@ -13,7 +17,7 @@ var heightPiece = 40;
 
 
 // (12 x 17)-(10 x 16)
-var gameTable =[
+var gameTable = [
 [1,0,0,0,0,0,0,0,0,0,0,1],
 [1,0,0,0,0,0,0,0,0,0,0,1],
 [1,0,0,0,0,0,0,0,0,0,0,1],
@@ -31,10 +35,30 @@ var gameTable =[
 [1,0,0,0,0,0,0,0,0,0,0,1],
 [1,0,0,0,0,0,0,0,0,0,0,1],
 [1,1,1,1,1,1,1,1,1,1,1,1]
-]
+];
 
 
-function inicializer(){
 
+function myFunction(){
+canvas = document.getElementById('canvas');
+ctx = canvas.getContext('2d');
 
+canvas.style.width = widthCanvas;
+canvas.style.height = heightCanvas;
 }
+
+setInterval(function(){
+ mainFunction();
+}, 1000/FPS);
+
+function cleanCanvas(){
+canvas.width = widthCanvas;
+canvas.height = heightCanvas;
+};
+
+
+function mainFunction(){
+    cleanCanvas();
+// console.log('bucle');
+
+};
